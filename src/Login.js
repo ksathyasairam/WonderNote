e3e665b6ad9b2828da41611ac51bd97a14fa512c
sattyboy <ksathyasairam@gmail.com>
2018-03-23 15:58:31 +0530

    Login Add

---
+++ src/Login.js
@@ -0,0 +1,45 @@
+//Login.js
+
+import React, {Component} from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import {Card, CardHeader,CardTitle, CardText} from 'material-ui/Card';
+import TextField from 'material-ui/TextField';
+import FlatButton from 'material-ui/FlatButton';
+import IconButton from 'material-ui/IconButton';
+
+
+class Login extends React.Component{
+
+	constructor(){
+
+		this.state={
+
+			props
+		}
+
+	}
+
+	render(){
+
+
+
+		return(
+
+		<div>
+		<h1> Login</h1>
+		</div>
+		);
+
+	}
+
+
+
+
+}
+
+const style = {
+  margin: 15,
+};
+
+
+export default Login;
